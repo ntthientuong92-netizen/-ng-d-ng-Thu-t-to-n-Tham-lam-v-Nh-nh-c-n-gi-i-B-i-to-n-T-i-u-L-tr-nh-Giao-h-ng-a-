b764420620c7f72551e2017ec230004e90f11cf2
ntthientuong92-netizen <ntthientuong9.2@gmail.com>
2026-09-09 16:35:58 +0700

    lenh code

---
+++ KhaiBao/ThuatToanNhanhCan.h
@@ -0,0 +1,9 @@
+#ifndef THUAT_TOAN_NHANH_CAN_H
+#define THUAT_TOAN_NHANH_CAN_H
+
+#include "CauTrucChung.h"
+#include <vector>
+
+KetQuaToiUu giaiNhanhCan(int N, const std::vector<std::vector<double>>& maTranKhoangCach);
+
+#endif
